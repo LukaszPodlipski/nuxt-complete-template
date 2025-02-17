39fc681f84476ba6f61c4b8b68996270b511a3a9
Łukasz Podlipski <lukaszpodlipskikontakt@gmail.com>
2025-02-17 11:19:48 +0100

    Set default color for app texts

--- app/components/TemplateWelcome.vue
+++ app/components/TemplateWelcome.vue
@@ -11,28 +11,26 @@
 <template>
   <div class="flex min-h-screen w-full items-center justify-center p-10">
     <div class="flex flex-col items-start p-4">
-      <h1 class="text-2xl font-bold text-white">Nuxt Template</h1>
+      <h1 class="text-secondary text-2xl font-bold">Nuxt Template</h1>
       <div class="flex flex-col gap-2">
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold text-white">Nuxt Icons</h2>
+          <h2 class="text-2xl font-bold">Nuxt Icons</h2>
           <div class="flex items-center gap-2">
             <Icon name="local:vue" class="text-secondary text-2xl" />
-            <span class="text-white">Local icon - Vue Icon module</span>
+            <span class="">Local icon - Vue Icon module</span>
           </div>
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold text-white">
-            PrimeVue Unstyled Hybrid Mode
-          </h2>
+          <h2 class="text-2xl font-bold">PrimeVue Unstyled Hybrid Mode</h2>
           <Button
-            class="bg-primary cursor-pointer rounded-sm px-2 py-3 text-white"
+            class="bg-primary cursor-pointer rounded-sm px-2 py-3"
             label="+ TailwindCSS"
           />
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold text-white">VeeValidate</h2>
+          <h2 class="text-2xl font-bold">VeeValidate</h2>
           <InputText v-model="value" type="text" />
           <small
             class="h-[15px]"
@@ -43,12 +41,12 @@
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold text-white">i18n</h2>
+          <h2 class="text-2xl font-bold">i18n</h2>
           <span class="text-secondary">{{ t('example') }}</span>
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold text-white">Nuxt Image</h2>
+          <h2 class="text-2xl font-bold">Nuxt Image</h2>
           <NuxtImg
             class="self-center"
             src="/nuxt-logo.png"
@@ -57,8 +55,8 @@
           />
         </div>
         <div class="app-separator" />
-        <div class="flex flex-col gap-3 text-white">
-          <h2 class="text-2xl font-bold text-white">And more:</h2>
+        <div class="flex flex-col gap-3">
+          <h2 class="text-2xl font-bold">And more:</h2>
           <span>- Husky</span>
           <span>- Vue use</span>
           <span>- Nuxt Viewport</span>
