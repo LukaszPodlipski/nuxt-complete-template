4412f2fefa241f1b98643a88df4630607809b5f8
Łukasz Podlipski <lukaszpodlipskikontakt@gmail.com>
2025-02-18 10:26:23 +0100

    Implement typography scss module

--- app/components/TemplateWelcome.vue
+++ app/components/TemplateWelcome.vue
@@ -28,26 +28,26 @@
     <div class="flex flex-col items-start p-4">
       <div class="flex flex-col gap-2">
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold">Nuxt Icons</h2>
+          <h2 class="text-h2-md">Nuxt Icons</h2>
           <div class="flex items-center gap-4">
-            <Icon name="local:vue" class="text-primary text-2xl" />
-            <span class="">Local icon - Vue Icon module</span>
+            <Icon name="local:vue" class="text-primary text-body-l" />
+            <span class="text-body-l">Local icon - Vue Icon module</span>
           </div>
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold">PrimeVue Unstyled Hybrid Mode</h2>
+          <h2 class="text-h2-md">PrimeVue Unstyled Hybrid Mode</h2>
           <Button
-            class="bg-primary cursor-pointer rounded-sm px-2 py-3"
+            class="bg-primary text-body-l cursor-pointer rounded-sm px-2 py-3"
             label="+ TailwindCSS"
           />
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold">VeeValidate</h2>
+          <h2 class="text-h2-md">VeeValidate</h2>
           <InputText v-model="value" type="text" :invalid="!!errorMessage" />
           <small
-            class="h-[15px]"
+            class="text-body-s"
             :class="errorMessage ? 'text-warning' : 'text-secondary'"
           >
             {{ errorMessage || 'Input valid' }}
@@ -55,12 +55,12 @@
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold">i18n</h2>
-          <span class="text-secondary">{{ t('example') }}</span>
+          <h2 class="text-h2-md">i18n</h2>
+          <span class="text-secondary text-body-m">{{ t('example') }}</span>
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold">Nuxt Image</h2>
+          <h2 class="text-h2-md">Nuxt Image</h2>
           <NuxtImg
             class="self-center"
             src="/nuxt-logo.png"
@@ -70,12 +70,12 @@
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold">Nuxt Fonts</h2>
-          <span class="text-secondary">Default font: Inter</span>
+          <h2 class="text-h2-md">Nuxt Fonts</h2>
+          <span class="text-secondary text-body-m">Default font: Inter</span>
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold">Pinia</h2>
+          <h2 class="text-h2-md">Pinia</h2>
           <NuxtImg
             class="self-center"
             src="https://pinia.vuejs.org/logo.svg"
@@ -85,18 +85,18 @@
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold">SSR External API fetch</h2>
-          <div v-if="formattedData" class="max-w-[300px]">
+          <h2 class="text-h2-md">SSR External API fetch</h2>
+          <div v-if="formattedData" class="text-body-m max-w-[300px]">
             {{ formattedData }}
           </div>
         </div>
         <div class="app-separator" />
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold">And more:</h2>
-          <span>- Husky</span>
-          <span>- Vue use</span>
-          <span>- Nuxt Viewport</span>
-          <span>- Eslint + Prettier</span>
+          <h2 class="text-h2-md">And more:</h2>
+          <span class="text-body-m">- Husky</span>
+          <span class="text-body-m">- Vue use</span>
+          <span class="text-body-m">- Nuxt Viewport</span>
+          <span class="text-body-m">- Eslint + Prettier</span>
         </div>
       </div>
     </div>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,7 +4,7 @@
       class="bg-background-secondary fixed flex w-full items-center justify-start gap-4 px-6 py-4"
     >
       <Icon name="local:vue" class="text-secondary text-3xl" />
-      <span class="text-secondary text-2xl font-bold">
+      <span class="text-secondary text-h2">
         Nuxt Complete Template - v4 compatible
       </span>
     </div>
